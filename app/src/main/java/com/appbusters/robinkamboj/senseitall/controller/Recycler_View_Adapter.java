9f83f2476d5d8ee70ed62d6bd2a9562ffa69826e
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-02-15 16:00:58 +0530

    recycler issue fixed

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/controller/Recycler_View_Adapter.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/controller/Recycler_View_Adapter.java
@@ -1,8 +1,8 @@
 package com.appbusters.robinkamboj.senseitall.controller;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +10,12 @@ import android.view.ViewGroup;
 import com.appbusters.robinkamboj.senseitall.R;
 import com.appbusters.robinkamboj.senseitall.model.Data;
 import com.appbusters.robinkamboj.senseitall.model.View_Holder;
+import com.squareup.picasso.Picasso;
 
 import java.util.Collections;
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class Recycler_View_Adapter extends RecyclerView.Adapter<View_Holder>{
 
@@ -36,14 +39,26 @@ public class Recycler_View_Adapter extends RecyclerView.Adapter<View_Holder>{
 
     @Override
     public void onBindViewHolder(final View_Holder holder, int position) {
+//        if(holder.sensor_imageview.i)
         _position = position;
         _holder = holder;
         holder.sensor_name.setText(list.get(_position).getSensor_name());
         holder.sensor_imageview.setImageResource(list.get(_position).getDrawable());
         if(!list.get(_position).isPresent){
-            holder.sensor_imageview.setBackgroundColor(_context.getResources().getColor(R.color.colorBlackShade));
-            holder.cardView.setClickable(false);
+//            holder.sensor_imageview.setBackgroundColor(_context.getResources().getColor(R.color.colorBlackShade));
+//            holder.cardView.setClickable(false);
+            Log.d(TAG, "onBindViewHolder: RED");
+
+            Picasso.with(context).load("http://www.fordesigner.com/imguploads/Image/cjbc/zcool/png20080526/1211771503.png")
+                    .fit()
+                    .into(holder.sensor_imageview);
+        }else{
+            Log.d(TAG, "onBindViewHolder: GREEN");
+            Picasso.with(context).load("http://www.fordesigner.com/imguploads/Image/cjbc/zcool/png20080526/1211771499.png")
+                    .fit()
+                    .into(holder.sensor_imageview);
         }
+
         holder.setClickListener(new ItemClickListener() {
             @Override
             public void onCLick(View v, int position, boolean isLongClick) {
--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/ListActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/ListActivity.java
@@ -1,8 +1,7 @@
 package com.appbusters.robinkamboj.senseitall.view;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
