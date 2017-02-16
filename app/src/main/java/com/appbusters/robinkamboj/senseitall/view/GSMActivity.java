8079ca4d9b97782afdecb1cc94080cec18236242
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-16 21:49:24 +0530

    GSM

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/GSMActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/GSMActivity.java
@@ -14,6 +14,9 @@ public class GSMActivity extends AppCompatActivity {
 
     String sensor_name;
     TextView textView;
+    TextView sim_operator_name, sim_country, sim_operator, sim_sn, sim_state, subscriber_id, mail_alpha_tag, mail_number,
+    icc_card, network_roaming, network_operator_name, network_country, network_operator, phone_type, cell_id,
+    location_area_code, call_state, line_one_number, device_id, software_version, data_activity, data_state, network_type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,30 @@ public class GSMActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        sim_operator_name = (TextView) findViewById(R.id.operator_name);
+        sim_country = (TextView) findViewById(R.id.country);
+        sim_operator = (TextView) findViewById(R.id.operator);
+        sim_sn = (TextView) findViewById(R.id.sn);
+        sim_state = (TextView) findViewById(R.id.state);
+        subscriber_id = (TextView) findViewById(R.id.subscriber_id);
+        mail_alpha_tag = (TextView) findViewById(R.id.mail_alpha_tag);
+        mail_number = (TextView) findViewById(R.id.mail_number);
+        icc_card = (TextView) findViewById(R.id.icc_card);
+        network_roaming = (TextView) findViewById(R.id.network_roaming);
+        network_operator_name = (TextView) findViewById(R.id.network_operator_name);
+        network_country = (TextView) findViewById(R.id.network_country);
+        network_operator = (TextView) findViewById(R.id.network_operator);
+        phone_type = (TextView) findViewById(R.id.phone_type);
+        cell_id = (TextView) findViewById(R.id.cell_id);
+        location_area_code = (TextView) findViewById(R.id.location_area_code);
+        call_state = (TextView) findViewById(R.id.call_state);
+        line_one_number = (TextView) findViewById(R.id.line_one_number);
+        device_id = (TextView) findViewById(R.id.device_id);
+        software_version = (TextView) findViewById(R.id.software_version);
+        data_activity = (TextView) findViewById(R.id.data_activity);
+        data_state = (TextView) findViewById(R.id.data_state);
+        network_type = (TextView) findViewById(R.id.network_type);
     }
 
     @Override
