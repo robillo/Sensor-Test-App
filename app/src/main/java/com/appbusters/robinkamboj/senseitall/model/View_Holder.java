7c43a0cf46f77f7796200fdd7ab435baa377081b
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-02-16 22:54:06 +0530

    battery act

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/model/View_Holder.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/model/View_Holder.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import com.appbusters.robinkamboj.senseitall.R;
 import com.appbusters.robinkamboj.senseitall.controller.ItemClickListener;
+import com.appbusters.robinkamboj.senseitall.view.BatteryActivity;
 import com.appbusters.robinkamboj.senseitall.view.CameraActivity;
 import com.appbusters.robinkamboj.senseitall.view.GSMActivity;
 
@@ -98,7 +99,9 @@ public class View_Holder extends RecyclerView.ViewHolder implements View.OnClick
                 break;
             }
             case 11:{
-
+                Intent i = new Intent(context, BatteryActivity.class);
+                i.putExtra("sensorName", sensorName);
+                context.startActivity(i);
                 break;
             }
             case 12:{
