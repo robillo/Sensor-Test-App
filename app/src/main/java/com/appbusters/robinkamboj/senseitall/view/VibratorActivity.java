99d75f3b5d1407f325335e339007b1f3e8694b5b
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-18 02:52:21 +0530

    Vibrator Done!

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/VibratorActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/VibratorActivity.java
@@ -1,9 +1,13 @@
 package com.appbusters.robinkamboj.senseitall.view;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.appbusters.robinkamboj.senseitall.R;
 
@@ -12,6 +16,8 @@ public class VibratorActivity extends AppCompatActivity {
 
     String sensor_name;
     TextView textView;
+    Context context;
+    Vibrator vib;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +28,17 @@ public class VibratorActivity extends AppCompatActivity {
         sensor_name = i.getStringExtra("sensorName");
         textView = (TextView) findViewById(R.id.textView);
         textView.setText(sensor_name);
+
+        context = getApplicationContext();
+
+        vib = (Vibrator) this.context.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.card:{
+                vib.vibrate(500);
+            }
+        }
     }
 }
