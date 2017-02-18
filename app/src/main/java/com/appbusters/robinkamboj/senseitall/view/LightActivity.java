1d3266a876bf987ffae2f7e49b490be78e8e1495
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-18 14:49:59 +0530

    Light Sensor Done!

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/LightActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/LightActivity.java
@@ -1,12 +1,16 @@
 package com.appbusters.robinkamboj.senseitall.view;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.appbusters.robinkamboj.senseitall.R;
@@ -15,6 +19,8 @@ import cn.fanrunqi.waveprogress.WaveProgressView;
 
 public class LightActivity extends AppCompatActivity {
 
+    String sensor_name;
+    TextView textView;
     WaveProgressView meter;
     TextView maximum, current;
     float max;
@@ -25,9 +31,19 @@ public class LightActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light);
 
+        Intent i = getIntent();
+        sensor_name = i.getStringExtra("sensorName");
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText(sensor_name);
+
         maximum = (TextView) findViewById(R.id.maximum);
         current = (TextView) findViewById(R.id.current);
         meter = (WaveProgressView) findViewById(R.id.meter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
 //        temp = (float) 0.5;
 //        meter.setProgress(temp);
@@ -48,7 +64,7 @@ public class LightActivity extends AppCompatActivity {
             if(sensorEvent.sensor.getType()==Sensor.TYPE_LIGHT){
                 float currentReading = sensorEvent.values[0];
                 current.setText(String.valueOf(currentReading));
-                String set = String.valueOf(String.format("%.2f",(currentReading/max)*100));
+                String set = String.valueOf(String.format("%.2f",(currentReading/max)*10000));
                 meter.setCurrent((int) currentReading, set);
             }
         }
@@ -58,4 +74,29 @@ public class LightActivity extends AppCompatActivity {
 
         }
     };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //nonspection SimplifiableIfStatement
+
+        switch (id){
+            case R.id.action_settings:{
+                break;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
