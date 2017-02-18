db0ce94607bc4328f922c522cdb3195133bd14c4
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-18 12:43:58 +0530

    CPU

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/CPUActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/CPUActivity.java
@@ -13,6 +13,11 @@ public class CPUActivity extends AppCompatActivity {
     String sensor_name;
     TextView textView;
     Context context;
+    TextView processor, number_of_cores, max_frequency, min_frequency, current_frequency, cpu_architecture, bogomips,
+             features, cpu_implementer, cpu_variant, cpu_part, cpu_revision, hardware, cpu_serial,
+             memory_size, memory_free, memory_used,
+             internal_size, internal_free, internal_used, external_size, external_free, external_used,
+             storage_size, partition_size1, partition_size2, partition_size3, partition_size4, partition_size5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
