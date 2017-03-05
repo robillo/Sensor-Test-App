8338f0114a33bb6f28dfb2c988670cf4bdc16a7c
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-05 16:30:43 +0530

    PROXIMITY

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/ProximityActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/ProximityActivity.java
@@ -1,13 +1,16 @@
 package com.appbusters.robinkamboj.senseitall.view;
 
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -20,8 +23,10 @@ public class ProximityActivity extends AppCompatActivity {
     private MisMeter meter;
     private SensorManager sensorManager;
     private Sensor sensor;
-    String sensor_name;
-    TextView textView;
+    private String sensor_name, results[];
+    private TextView textView;
+
+    private TextView name, vendor, wake_up, minimum_delay, version, power, maximum_range, resolution;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +39,54 @@ public class ProximityActivity extends AppCompatActivity {
         meter = (MisMeter) findViewById(R.id.meter);
         meter.setProgress(1);
 
+        name = (TextView) findViewById(R.id.name);
+        vendor = (TextView) findViewById(R.id.vendor);
+
+        minimum_delay = (TextView) findViewById(R.id.minimum_delay);
+        version = (TextView) findViewById(R.id.version);
+        power = (TextView) findViewById(R.id.power);
+        maximum_range = (TextView) findViewById(R.id.maximum_range);
+        resolution = (TextView) findViewById(R.id.resolution);
+        wake_up = (TextView) findViewById(R.id.wake_up);
+
         Intent i = getIntent();
         sensor_name = i.getStringExtra("sensorName");
         textView = (TextView) findViewById(R.id.textView);
         textView.setText(sensor_name);
+
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                setResults();
+                setTextViews();
+            }
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-
         sensorManager.registerListener(proximitySensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void setResults(){
+        results = new String[]{sensor.getName(), sensor.getVendor(), (sensor.isWakeUpSensor())?"true":"false",
+                String.valueOf(sensor.getMinDelay()), String.valueOf(sensor.getVersion()),
+                String.valueOf(sensor.getPower()), String.valueOf(sensor.getMaximumRange()),
+                String.valueOf(sensor.getResolution())};
+    }
+
+    private void setTextViews(){
+        name.setText(results[0]);
+        vendor.setText(results[1]);
+        wake_up.setText(results[2]);
+        minimum_delay.setText(results[3]);
+        wake_up.setText(results[4]);
+        power.setText(results[5]);
+        maximum_range.setText(results[6]);
+        resolution.setText(results[7]);
     }
 
     SensorEventListener proximitySensorEventListener = new SensorEventListener() {
