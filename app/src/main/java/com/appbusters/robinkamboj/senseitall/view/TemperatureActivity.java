da559152607a2031170a2635882787b5aacc5290
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-12 14:17:50 +0530

    Pressure And Temperature.

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/TemperatureActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/TemperatureActivity.java
@@ -52,13 +52,24 @@ public class TemperatureActivity extends AppCompatActivity {
 
     private void setResults(){
         results = new String[]{
-                sensor.getName(), sensor.getVendor(), String.valueOf(sensor.getVersion()), String.valueOf(sensor.getMaximumRange()),
-                String.valueOf(sensor.getPower()),
-                String.valueOf(sensor.getMinDelay()), String.valueOf(sensor.getMaxDelay()), String.valueOf(sensor.getResolution())
+                "Name:    " + sensor.getName(), "Vendor:    " + sensor.getVendor(),
+                "Version:    " + String.valueOf(sensor.getVersion()),
+                "Maximum Range:    " + String.valueOf(sensor.getMaximumRange()),
+                "Power:    " + String.valueOf(sensor.getPower()),
+                "Minimum Delay:    " + String.valueOf(sensor.getMinDelay()),
+                "Maximum Delay:    " + String.valueOf(sensor.getMaxDelay()),
+                "Resolution:    " + String.valueOf(sensor.getResolution())
         };
     }
 
     private void setTextviews(){
-
+        name.setText(results[0]);
+        vendor.setText(results[1]);
+        version.setText(results[2]);
+        maximum_range.setText(results[3]);
+        power.setText(results[4]);
+        minimum_delay.setText(results[5]);
+        maximum_delay.setText(results[6]);
+        resolution.setText(results[7]);
     }
 }
