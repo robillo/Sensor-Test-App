df7731174739bfe2c74df2f5c543261212aa6395
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-13 22:28:32 +0530

    New Vibrator Layout.

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/robin/VibratorActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/robin/VibratorActivity.java
@@ -2,10 +2,13 @@ package com.appbusters.robinkamboj.senseitall.view.robin;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.Vibrator;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,11 +16,12 @@ import com.appbusters.robinkamboj.senseitall.R;
 
 public class VibratorActivity extends AppCompatActivity {
 
-
-    String sensor_name;
-    TextView textView;
-    Context context;
-    Vibrator vib;
+    private RelativeLayout activity_vibrate;
+    private String sensor_name;
+    private TextView textView;
+    private Context context;
+    private Vibrator vib;
+    private boolean loop = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,7 @@ public class VibratorActivity extends AppCompatActivity {
 
         context = getApplicationContext();
 
+        activity_vibrate = (RelativeLayout) findViewById(R.id.activity_vibrate);
         vib = (Vibrator) this.context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
@@ -38,6 +43,29 @@ public class VibratorActivity extends AppCompatActivity {
         switch(v.getId()){
             case R.id.card:{
                 vib.vibrate(500);
+                break;
+            }
+            case R.id.card2:{
+
+                break;
+            }
+            case R.id.card3:{
+
+                break;
+            }
+            case R.id.card4:{
+
+
+
+
+                Snackbar.make(activity_vibrate, "Click To Exit the Vibrate Loop.", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Exit", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                loop = false;
+                            }
+                        }).show();
+                break;
             }
         }
     }
