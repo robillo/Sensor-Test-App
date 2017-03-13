382df69b8d5a9158033ccc57bdba848af32a18f9
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-13 21:29:03 +0530

    ...

---
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/robin/MotionDetectorActivity.java
@@ -0,0 +1,15 @@
+package com.appbusters.robinkamboj.senseitall.view.robin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appbusters.robinkamboj.senseitall.R;
+
+public class MotionDetectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motion_detector);
+    }
+}
