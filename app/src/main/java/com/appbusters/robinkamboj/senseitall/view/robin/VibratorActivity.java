fd753d7b0ac70543350eae682926c285a89e1eec
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-14 22:23:32 +0530

    Vibrator Layout-land

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/robin/VibratorActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/robin/VibratorActivity.java
@@ -7,6 +7,8 @@ import android.os.Vibrator;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -128,5 +130,30 @@ public class VibratorActivity extends AppCompatActivity {
     public void onBackPressed() {
         timer.cancel();
         super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //nonspection SimplifiableIfStatement
+
+        switch (id){
+            case R.id.action_settings:{
+                break;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
