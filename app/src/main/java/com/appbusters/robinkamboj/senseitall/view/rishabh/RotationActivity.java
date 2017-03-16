31833d67ce771f0407423c371b15621dd453caad
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-16 10:58:32 +0530

    barometer

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/RotationActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/RotationActivity.java
@@ -6,7 +6,9 @@ import android.hardware.SensorManager;
 import android.hardware.TriggerEvent;
 import android.hardware.TriggerEventListener;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.appbusters.robinkamboj.senseitall.R;
 
@@ -14,8 +16,11 @@ public class RotationActivity extends AppCompatActivity {
 
 
     private SensorManager mSensorManager;
-    private Sensor mSensor;
+    private String results[];
+    private Sensor sensor;
     TriggerEventListener mTriggerEventListener;
+    private TextView name, vendor, version, maximum_range, power, minimum_delay, maximum_delay, resolution;
+
 //    private Cube mCube;
 
     private final float[] mRotationMatrix = new float[16];
@@ -26,7 +31,7 @@ public class RotationActivity extends AppCompatActivity {
 
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
 
         mTriggerEventListener = new TriggerEventListener() {
             @Override
@@ -34,18 +39,58 @@ public class RotationActivity extends AppCompatActivity {
                 // Do work
             }
         };
-        mSensorManager.requestTriggerSensor(mTriggerEventListener, mSensor);
+        name = (TextView) findViewById(R.id.name);
+        vendor = (TextView) findViewById(R.id.vendor);
+        version = (TextView) findViewById(R.id.version);
+        maximum_range = (TextView) findViewById(R.id.maximum_range);
+        power = (TextView) findViewById(R.id.power);
+        minimum_delay = (TextView) findViewById(R.id.minimum_delay);
+        maximum_delay = (TextView) findViewById(R.id.maximum_delay);
+        resolution = (TextView) findViewById(R.id.resolution);
+
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                setResults();
+                setTextviews();
+            }
+        });
+        mSensorManager.requestTriggerSensor(mTriggerEventListener, sensor);
+    }
+
+    private void setResults(){
+        results = new String[]{
+                "Name:    " + sensor.getName(), "Vendor:    " + sensor.getVendor(),
+                "Version:    " + String.valueOf(sensor.getVersion()),
+                "Maximum Range:    " + String.valueOf(sensor.getMaximumRange()),
+                "Power:    " + String.valueOf(sensor.getPower()),
+                "Minimum Delay:    " + String.valueOf(sensor.getMinDelay()),
+                "Maximum Delay:    " + String.valueOf(sensor.getMaxDelay()),
+                "Resolution:    " + String.valueOf(sensor.getResolution())
+        };
+    }
+
+    private void setTextviews(){
+        name.setText(results[0]);
+        vendor.setText(results[1]);
+        version.setText(results[2]);
+        maximum_range.setText(results[3]);
+        power.setText(results[4]);
+        minimum_delay.setText(results[5]);
+        maximum_delay.setText(results[6]);
+        resolution.setText(results[7]);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.requestTriggerSensor(mTriggerEventListener, mSensor);
+        mSensorManager.requestTriggerSensor(mTriggerEventListener, sensor);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mSensorManager.cancelTriggerSensor(mTriggerEventListener, mSensor);
+        mSensorManager.cancelTriggerSensor(mTriggerEventListener, sensor);
     }
 }
