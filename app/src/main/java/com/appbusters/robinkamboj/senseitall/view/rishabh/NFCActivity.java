4b93ebde9aae9f34d872a9edccd1f631fb4b9c2e
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-17 02:09:54 +0530

    nfc

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/NFCActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/NFCActivity.java
@@ -5,11 +5,14 @@ import android.content.pm.PackageManager;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcManager;
 import android.os.Bundle;
+import android.os.HardwarePropertiesManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.appbusters.robinkamboj.senseitall.R;
 
 public class NFCActivity extends AppCompatActivity {
+    private static final String TAG = "NFC";
     NfcAdapter nfcAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,10 @@ public class NFCActivity extends AppCompatActivity {
         NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
         boolean isen = nfcAdapter.isEnabled();
-
+        Log.d(TAG, "onCreate: "+nfcManager.toString());
+        Log.d(TAG, "onCreate: "+nfcAdapter.isNdefPushEnabled());
+        Log.d(TAG, "onCreate: "+nfcAdapter.isEnabled());
+//        Log.d(TAG, "onCreate: "+nfcAdapter.invokeBeam(this));
+        Log.d(TAG, "onCreate: "+nfcAdapter.toString());
     }
 }
