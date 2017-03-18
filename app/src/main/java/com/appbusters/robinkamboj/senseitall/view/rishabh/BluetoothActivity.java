728500d8afd94234b29274938b74cf11411b4e06
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-18 13:36:00 +0530

    Bluetooth

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/BluetoothActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/BluetoothActivity.java
@@ -1,28 +1,57 @@
 package com.appbusters.robinkamboj.senseitall.view.rishabh;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.SensorManager;
 import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.appbusters.robinkamboj.senseitall.R;
+
+import java.util.Set;
 
 public class BluetoothActivity extends AppCompatActivity {
 
     private static final int BLUETOOTH_PERMISSION = 120;
+    SensorManager sensorManager;
+    BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
-        String[] permissions={Manifest.permission.BLUETOOTH};
+        String[] permissions={Manifest.permission.BLUETOOTH,Manifest.permission.BLUETOOTH_ADMIN};
 
         if(!hasPermissions(this, permissions)){
             ActivityCompat.requestPermissions(this, permissions, BLUETOOTH_PERMISSION);
         }
+//        sensorManager = (SensorManager) getSystemService(BLUETOOTH_SERVICE);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.enable();
+        }
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+                if (pairedDevices.size() > 0) {
+                    // There are paired devices. Get the name and address of each paired device.
+                    for (BluetoothDevice device : pairedDevices) {
+                        String deviceName = device.getName();
+                        String deviceHardwareAddress = device.getAddress(); // MAC address
+                    }
+                }
+            }
+        }, 1500);
 
     }
     public static boolean hasPermissions(Context context, String[] permissions){
@@ -34,5 +63,13 @@ public class BluetoothActivity extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    @Override
+    protected void onStop() {
+        if (bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.disable();
+        }
+        super.onStop();
     }
 }
