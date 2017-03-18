fb8f7618e89db71ecda11a36766a6334c9b42227
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-18 17:17:17 +0530

    GPS

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/MapsActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/MapsActivity.java
@@ -1,33 +1,57 @@
 package com.appbusters.robinkamboj.senseitall.view.rishabh;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 import com.appbusters.robinkamboj.senseitall.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
+    private static final String TAG = "GPs";
     private GoogleMap mMap;
-    double Lat=28;
-    double Lon=77;
+    double Lat=0;
+    double Lon=0;
+    LatLng latLng;
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
+    Marker currLocationMarker;
+    GoogleMap mGoogleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        Intent i = getIntent();
+
+
+        final Intent i = getIntent();
         Lat  = i.getDoubleExtra("lat",Lat);
         Lon  = i.getDoubleExtra("lon",Lon);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+            mapFragment.getMapAsync(this);
     }
 
 
@@ -42,16 +66,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        mGoogleMap = googleMap;
+        mGoogleMap.setMyLocationEnabled(true);
 
-        // Add a marker in Sydney and move the camera
+//        buildGoogleApiClient();
 
-        LatLng Delhi = new LatLng(Lat, Lon);
-        mMap.addMarker(new MarkerOptions().position(Delhi).title("Delhi"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(Delhi));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-        mMap.getUiSettings().setZoomControlsEnabled(true);
-        mMap.getUiSettings().setMapToolbarEnabled(true);
+        mGoogleApiClient.connect();
+
+
+//        mMap.addMarker(new MarkerOptions().position(Delhi).title("Delhi"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(Delhi));
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+//        mMap.getUiSettings().setZoomControlsEnabled(true);
+//        mMap.getUiSettings().setMapToolbarEnabled(true);
 //        mMap.addCircle(new CircleOptions());
     }
-}
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            //place marker at current position
+            //mGoogleMap.clear();
+            latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            markerOptions.title("Current Position");
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+            currLocationMarker = mGoogleMap.addMarker(markerOptions);
+        }
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        //mLocationRequest.setSmallestDisplacement(0.1F); //1/10 meter
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+    }
