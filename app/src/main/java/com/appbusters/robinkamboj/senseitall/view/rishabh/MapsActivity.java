7ca36da6d90dad0d6613948de4b281abcb3e9f94
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-19 10:29:58 +0530

    comp

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/MapsActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/MapsActivity.java
@@ -54,7 +54,6 @@ public class MapsActivity extends FragmentActivity implements
         Toast.makeText(this, "Click on Marker to know Latitude and Longitude", Toast.LENGTH_SHORT).show();
     }
 
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
