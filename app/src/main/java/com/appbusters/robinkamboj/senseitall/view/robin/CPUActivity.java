3453703e380b3de0dea05a802bb93dbba5a36a41
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-22 13:51:34 +0530

    CPU Half Done.

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/robin/CPUActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/robin/CPUActivity.java
@@ -24,10 +24,9 @@ public class CPUActivity extends AppCompatActivity {
     Process process ;
     byte[] byteArry ;
 
-    TextView processor, number_of_cores, max_frequency, min_frequency, current_frequency, cpu_architecture, bogomips,
-             features, cpu_implementer, cpu_variant, cpu_part, cpu_revision, hardware, cpu_serial,
-             memory_size, memory_free, memory_used,
-             internal_size, internal_free, internal_used, external_size, external_free, external_used,
+    TextView memory_size, memory_free, memory_used,
+             internal_size, internal_free, internal_used,
+            external_size, external_free, external_used,
              storage_size, partition_size1, partition_size2, partition_size3, partition_size4, partition_size5;
 
     @Override
@@ -50,20 +49,6 @@ public class CPUActivity extends AppCompatActivity {
         }
         cpu_all.setText(Holder);
 
-//        processor = (TextView) findViewById(R.id.processor);
-//        number_of_cores = (TextView) findViewById(R.id.cores);
-//        max_frequency = (TextView) findViewById(R.id.max_frequency);
-//        min_frequency = (TextView) findViewById(R.id.min_frequency);
-//        current_frequency = (TextView) findViewById(R.id.current_frequency);
-//        cpu_architecture = (TextView) findViewById(R.id.cpu_architecture);
-//        bogomips = (TextView) findViewById(R.id.bogomips);
-//        features = (TextView) findViewById(R.id.features);
-//        cpu_implementer = (TextView) findViewById(R.id.cpu_implementer);
-//        cpu_variant = (TextView) findViewById(R.id.cpu_variant);
-//        cpu_part = (TextView) findViewById(R.id.cpu_part);
-//        cpu_revision = (TextView) findViewById(R.id.cpu_revision);
-//        hardware = (TextView) findViewById(R.id.hardware);
-//        cpu_serial = (TextView) findViewById(R.id.serial);
         memory_size = (TextView) findViewById(R.id.memory_size);
         memory_free = (TextView) findViewById(R.id.memory_free);
         memory_used = (TextView) findViewById(R.id.memory_used);
