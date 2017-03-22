7353f28236904cdefabe818cf3e6c47a01fd956d
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-23 00:02:54 +0530

    bug

---
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/WiFiActivity.java
@@ -0,0 +1,18 @@
+package com.appbusters.robinkamboj.senseitall.view.rishabh;
+
+import android.net.wifi.WifiInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appbusters.robinkamboj.senseitall.R;
+
+public class WiFiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wi_fi);
+
+//        WifiInfo wifiInfo = new WifiInfo();
+    }
+}
