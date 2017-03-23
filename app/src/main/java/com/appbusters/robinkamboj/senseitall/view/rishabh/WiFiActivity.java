a0366deb865d6d6189f959a01e1f474c765248b8
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-23 22:03:57 +0530

    wifi almost done

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/WiFiActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/WiFiActivity.java
@@ -1,14 +1,22 @@
 package com.appbusters.robinkamboj.senseitall.view.rishabh;
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.widget.TextView;
 
 import com.appbusters.robinkamboj.senseitall.R;
+import com.appbusters.robinkamboj.senseitall.model.BTInfo;
+
+import java.util.List;
+import java.util.Set;
 
 import static android.content.Context.*;
 
@@ -37,29 +45,61 @@ public class WiFiActivity extends AppCompatActivity {
 
         wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
 
-        if(wifiManager.getWifiState()==WifiManager.WIFI_STATE_DISABLED){
-            state.setText("State:    Disabled" );
-        }else if(wifiManager.getWifiState()==WifiManager.WIFI_STATE_DISABLING){
-            state.setText("State:    Disabling" );
-        }else if(wifiManager.getWifiState()==WifiManager.WIFI_STATE_ENABLED){
-            state.setText("State:    Enabled" );
-        }else if(wifiManager.getWifiState()==WifiManager.WIFI_STATE_ENABLING){
-            state.setText("State:    Enabling" );
-        }else if(wifiManager.getWifiState()==WifiManager.WIFI_STATE_UNKNOWN){
-            state.setText("State:    UNKNOWN" );
+        final ProgressDialog progress = new ProgressDialog(WiFiActivity.this);
+        progress.setMessage("Getting WIFI Info...");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setIndeterminate(true);
+        progress.show();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                setResults();
+                setTextviews();
+
+                List<WifiConfiguration> configDevices = wifiManager.getConfiguredNetworks();
+
+
+                if (configDevices.size() > 0) {
+                    for (WifiConfiguration device : configDevices) {
+
+
+//                        String deviceName = device.getName();
+//                        String deviceHardwareAddress = device.getAddress(); // MAC address
+//
+//                        Log.d(TAG, "run: "+deviceName);
+//                        Log.d(TAG, "run: "+deviceHardwareAddress);
+
+                    }
+                }
+                progress.hide();
+
+//                BluetoothActivity.RVAdapter ad = new RVAdapter();
+//
+//                rv.setLayoutManager(new LinearLayoutManager(WiFiActivity.this));
+//
+//                rv.setAdapter(ad);
+
+            }
+        }, 1500);
+
+        if (wifiManager.getWifiState() == WifiManager.WIFI_STATE_DISABLED) {
+            state.setText("State:    Disabled");
+        } else if (wifiManager.getWifiState() == WifiManager.WIFI_STATE_DISABLING) {
+            state.setText("State:    Disabling");
+        } else if (wifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLED) {
+            state.setText("State:    Enabled");
+        } else if (wifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLING) {
+            state.setText("State:    Enabling");
+        } else if (wifiManager.getWifiState() == WifiManager.WIFI_STATE_UNKNOWN) {
+            state.setText("State:    UNKNOWN");
         }
 
         wifiInfo = wifiManager.getConnectionInfo();
 
 
-        Handler handler = new Handler();
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                setResults();
-                setTextviews();
-            }
-        });
     }
 
 
