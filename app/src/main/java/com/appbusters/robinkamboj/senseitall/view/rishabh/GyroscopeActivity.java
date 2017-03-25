612c92a4cf5e96201ad5034e34f985c9910ca5c3
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-25 08:57:08 +0530

    gyroscope

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/GyroscopeActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/GyroscopeActivity.java
@@ -1,17 +1,24 @@
 package com.appbusters.robinkamboj.senseitall.view.rishabh;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.appbusters.robinkamboj.senseitall.R;
 
 public class GyroscopeActivity extends AppCompatActivity {
 
-
+    SensorManager sensorManager;
+    Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gyroscope);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED);
+
     }
 }
