d717a3ec8163eb70f37303eb7f1e7332c3236f9b
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-28 21:45:16 +0530

    usb

---
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/USBActivity.java
@@ -0,0 +1,17 @@
+package com.appbusters.robinkamboj.senseitall.view.rishabh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appbusters.robinkamboj.senseitall.R;
+
+public class USBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usb);
+
+        
+    }
+}
