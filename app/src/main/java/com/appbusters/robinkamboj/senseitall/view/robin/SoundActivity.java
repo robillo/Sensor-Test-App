b1422ee812218cb52c6d151415e01f5f52798e48
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-02 21:17:09 +0530

    basic layout

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/robin/SoundActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/robin/SoundActivity.java
@@ -2,14 +2,26 @@ package com.appbusters.robinkamboj.senseitall.view.robin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 import com.appbusters.robinkamboj.senseitall.R;
 
 public class SoundActivity extends AppCompatActivity {
 
+    private ImageButton play, pause;
+    private SeekBar volume;
+    private ImageView bg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
+
+        play = (ImageButton) findViewById(R.id.play);
+        pause = (ImageButton) findViewById(R.id.pause);
+        volume = (SeekBar) findViewById(R.id.volume);
+        bg = (ImageView) findViewById(R.id.bg);
     }
 }
