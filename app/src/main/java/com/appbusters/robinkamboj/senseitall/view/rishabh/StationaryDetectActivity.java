c91de6fc398efdb07257a05f4ded080fb52312dc
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-04-04 22:43:48 +0530

    stationary detect

---
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/rishabh/StationaryDetectActivity.java
@@ -0,0 +1,15 @@
+package com.appbusters.robinkamboj.senseitall.view.rishabh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appbusters.robinkamboj.senseitall.R;
+
+public class StationaryDetectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stationary_detect);
+    }
+}
