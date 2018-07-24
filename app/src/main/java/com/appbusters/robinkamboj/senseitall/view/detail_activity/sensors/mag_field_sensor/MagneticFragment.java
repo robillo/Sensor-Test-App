d9a7388be456648fb92bf705e74fd6c500cdaf56
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-07-25 04:04:41 +0530

    added all sensors to detail activity

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/detail_activity/sensors/mag_field_sensor/MagneticFragment.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/detail_activity/sensors/mag_field_sensor/MagneticFragment.java
@@ -1,18 +1,24 @@
 package com.appbusters.robinkamboj.senseitall.view.detail_activity.sensors.mag_field_sensor;
 
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.appbusters.robinkamboj.senseitall.R;
+import com.appbusters.robinkamboj.senseitall.view.detail_activity.abstract_stuff.SensorFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MagneticFragment extends Fragment {
+public class MagneticFragment extends SensorFragment implements MagneticFieldInterface {
 
 
     public MagneticFragment() {
@@ -21,10 +27,37 @@ public class MagneticFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_magnetic, container, false);
+        View v = inflater.inflate(R.layout.fragment_magnetic, container, false);
+        setup(v);
+        return v;
     }
 
+    @Override
+    public void initializeSensor() {
+        SensorManager sensorManager = null;
+
+        if(getActivity() != null)
+            sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+
+        if(sensorManager != null)
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    public void initializeBasicInformation() {
+        addToDetailsList(sensorDetails, "Vendor", sensor.getVendor());
+        addToDetailsList(sensorDetails, "Resolution", String.valueOf(sensor.getResolution()));
+        addToDetailsList(sensorDetails, "Minimum Delay", String.valueOf(sensor.getMinDelay()));
+        addToDetailsList(sensorDetails, "Maximum Delay", String.valueOf(sensor.getMaxDelay()));
+        addToDetailsList(sensorDetails, "Power", String.valueOf(sensor.getPower()));
+        addToDetailsList(sensorDetails, "Maximum Range", String.valueOf(sensor.getMaximumRange()));
+        addToDetailsList(sensorDetails, "Version", String.valueOf(sensor.getVersion()));
+        addToDetailsList(sensorDetails, "Is Wake Up Sensor", String.valueOf(sensor.isWakeUpSensor()));
+        addToDetailsList(sensorDetails, "Reporting Mode", String.valueOf(sensor.getReportingMode()));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            addToDetailsList(sensorDetails, "Is Dynamic Sensor", String.valueOf(sensor.isDynamicSensor()));
+    }
 }
