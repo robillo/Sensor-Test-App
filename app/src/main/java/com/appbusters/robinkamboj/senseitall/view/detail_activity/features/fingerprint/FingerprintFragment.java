f54a80252fca5b33560adc32c1f7c9d63db37033
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-07-25 11:59:29 +0530

    fingerprint fragment

--- app/src/main/java/com/appbusters/robinkamboj/senseitall/view/detail_activity/features/fingerprint/FingerprintFragment.java
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/detail_activity/features/fingerprint/FingerprintFragment.java
@@ -1,6 +1,10 @@
 package com.appbusters.robinkamboj.senseitall.view.detail_activity.features.fingerprint;
 
 
+import android.annotation.SuppressLint;
+import android.app.KeyguardManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -15,11 +19,16 @@ import com.appbusters.robinkamboj.senseitall.view.detail_activity.abstract_stuff
 
 import butterknife.ButterKnife;
 
+import static android.content.Context.FINGERPRINT_SERVICE;
+import static android.content.Context.KEYGUARD_SERVICE;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FingerprintFragment extends FeatureFragment implements FingerprintInterface {
 
+    private KeyguardManager keyguardManager;
+    private FingerprintManager fingerprintManager;
 
     public FingerprintFragment() {
         // Required empty public constructor
@@ -39,26 +48,41 @@ public class FingerprintFragment extends FeatureFragment implements FingerprintI
     public void setup(View v) {
         ButterKnife.bind(this, v);
         initializeSensor();
-//        if(sensor == null) {
-//            Toast.makeText(getActivity(), "Failed to load sensor.", Toast.LENGTH_SHORT).show();
-//            if(getActivity() != null) getActivity().onBackPressed();
-//        }
-//        else {
-//            showSensorDetails();
-//        }
 
         hideGoToTestIfNoTest();
 
         setupAbout();
+
+        showSensorDetails();
     }
 
+    @SuppressLint("NewApi")
     @Override
     public void initializeSensor() {
-
+        if(getActivity() == null) return;
+        keyguardManager = (KeyguardManager) getActivity().getSystemService(KEYGUARD_SERVICE);
+        fingerprintManager = (FingerprintManager) getActivity().getSystemService(FINGERPRINT_SERVICE);
     }
 
     @Override
     public void initializeBasicInformation() {
+        if(keyguardManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+                addToDetailsList(sensorDetails, "Is Keyguard Locked", String.valueOf(keyguardManager.isKeyguardLocked()));
+                addToDetailsList(sensorDetails, "Is Keyguard Secure", String.valueOf(keyguardManager.isKeyguardSecure()));
+                addToDetailsList(sensorDetails, "Is Device Locked", String.valueOf(keyguardManager.isDeviceLocked()));
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                addToDetailsList(sensorDetails, "Is Device Secure", String.valueOf(keyguardManager.isDeviceSecure()));
+            }
+        }
+
+        if(fingerprintManager == null) return;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            addToDetailsList(sensorDetails, "Is Hardware Detected", String.valueOf(fingerprintManager.isHardwareDetected()));
+            addToDetailsList(sensorDetails, "Has Enrolled Fingerprint", String.valueOf(fingerprintManager.hasEnrolledFingerprints()));
+        }
 
     }
 }
