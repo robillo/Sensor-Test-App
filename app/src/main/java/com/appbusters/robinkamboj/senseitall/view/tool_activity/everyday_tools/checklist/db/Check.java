0f5e6742282729167fb4ac8f5fef4b0130001a02
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-10-05 16:50:39 +0530

    check db

---
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/tool_activity/everyday_tools/checklist/db/Check.java
@@ -0,0 +1,44 @@
+package com.appbusters.robinkamboj.senseitall.view.tool_activity.everyday_tools.checklist.db;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "check")
+public class Check {
+
+    public Check() {}
+
+    public Check(String check_text, Boolean isDone) {
+        this.check_text = check_text;
+        this.isDone = isDone;
+    }
+
+    @PrimaryKey(autoGenerate = true)
+    private int id = 0;
+    private String check_text;
+    private Boolean isDone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCheck_text() {
+        return check_text;
+    }
+
+    public void setCheck_text(String check_text) {
+        this.check_text = check_text;
+    }
+
+    public Boolean getDone() {
+        return isDone;
+    }
+
+    public void setDone(Boolean done) {
+        isDone = done;
+    }
+}
---
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/tool_activity/everyday_tools/checklist/db/CheckDao.java
@@ -0,0 +1,34 @@
+package com.appbusters.robinkamboj.senseitall.view.tool_activity.everyday_tools.checklist.db;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
+
+@Dao
+public interface CheckDao {
+
+    @Insert(onConflict = REPLACE)
+    void insertChecks(Check... checks);
+
+    @Delete
+    void deleteChecks(Check... checks);
+
+    @Query("DELETE FROM `check` WHERE id = :id")
+    void deleteCheckById(int... id);
+
+    @Query("UPDATE `check` SET check_text = :check_text, isDone = :isDone WHERE id = :id")
+    void updateCheckById(String check_text, boolean isDone, int id);
+
+    @Query("UPDATE `check` SET isDone = :isDone WHERE id = :id")
+    void updateIsDoneById(boolean isDone, int id);
+
+    @Query("SELECT * FROM `check`")
+    LiveData<List<Check>> getAllChecks();
+
+}
