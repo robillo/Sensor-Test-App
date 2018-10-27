fef30ebfa62187250794f2849ce46b439dfcb1b4
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-10-27 23:25:26 +0530

    started working on new dashboard screen

---
+++ app/src/main/java/com/appbusters/robinkamboj/senseitall/view/tab_dashboard_activity/TabMainActivity.kt
@@ -0,0 +1,13 @@
+package com.appbusters.robinkamboj.senseitall.view.tab_dashboard_activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.appbusters.robinkamboj.senseitall.R
+
+class TabMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab_main)
+    }
+}
